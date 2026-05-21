b39039a27017660bba6f33463e7413e032f9a6d6
Tuguldur <B211910811@must.edu.mn>
2026-05-21 20:50:17 +0800

    zurag zassan4

--- src/tabs/Durslel.jsx
+++ src/tabs/Durslel.jsx
@@ -41,11 +41,11 @@ const SLIDES = [
 
 function Scene({ type, bg }) {
   const images = {
-    road: "public/images/road.png",
-    tow: "public/images/tow.png",
-    money: "public/images/money.png",
-    hard: "public/images/life.png",
-    letter: "public/images/tugsgul.png",
+    road: "/images/road.png",
+    tow: "/images/tow.png",
+    money: "/images/money.png",
+    hard: "/images/life.png",
+    letter: "/images/tugsgul.png",
   };
 
   return (
